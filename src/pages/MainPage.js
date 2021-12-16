1e04231418445d85ee62cba9870d7f26aa9cd3d3
top-dev-999 <topdeveloper1999@outlook.com>
2021-12-17 02:25:27 +0800

    fix locked

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,53 +1,61 @@
 import React from "react";
 
-import { Grid, List, ListItem, ListItemText } from '@mui/material';
+import { Grid, List, ListItem, ListItemText } from "@mui/material";
 //import Css
-import './MainPage.css';
+import "./MainPage.css";
 class MainPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            temperature: props.temperature,
-            comfort: props.comfort,
-            alarm: props.alarm,
-            door: props.door,
-            garage: props.garage
-        }
-    }
-    render() {
-        return (
-            <div className="main-control">
-                <h1>Sample App</h1>
-                <Grid container spacing={2}>
-                  
-                    <Grid item xs={6}>
-                        <header>Comfort</header>
-                        <div>{this.state.temperature}`F</div>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <div>System: {this.state.comfort}</div>
-                    </Grid>
-                   
-                    <Grid item xs={12}>
-                        <div>Security</div>
-                       
-                        <List>
-                            <ListItem>
-                                <ListItemText primary="Instrusion Alarm" secondary={this.state.alarm === true ? "Open" : "Locked"} />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText primary="Doors" secondary={this.state.door === true ? "Open" : "Locked"} />
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText primary="Garage" secondary={this.state.garage === true ? "Open" : "Locked"} />
-                            </ListItem>
-                        </List>
-                    </Grid>
-                </Grid>
-                <div></div>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      temperature: props.temperature,
+      comfort: props.comfort,
+      alarm: props.alarm,
+      door: props.door,
+      garage: props.garage,
+    };
+  }
+  render() {
+    return (
+      <div className="main-control">
+        <h1>Sample App</h1>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <header>Comfort</header>
+            <div>{this.state.temperature}`F</div>
+          </Grid>
+          <Grid item xs={6}>
+            <div>System: {this.state.comfort}</div>
+          </Grid>
+
+          <Grid item xs={12}>
+            <div>Security</div>
+
+            <List>
+              <ListItem>
+                <ListItemText
+                  primary="Instrusion Alarm"
+                  secondary={this.state.alarm === true ? "Locked" : "Open"}
+                />
+              </ListItem>
+              <ListItem>
+                <ListItemText
+                  primary="Doors"
+                  secondary={this.state.door === true ? "Locked" : "Open"}
+                />
+              </ListItem>
+              <ListItem>
+                <ListItemText
+                  primary="Garage"
+                  secondary={this.state.garage === true ? "Locked" : "Open"}
+                />
+              </ListItem>
+            </List>
+          </Grid>
+        </Grid>
+        <div></div>
+      </div>
+    );
+  }
 }
 
-export default MainPage
+export default MainPage;
